c74ef29c0a6fc2e7a253ad86724dd0be4cebce8c
Drgil666 <2574105038@qq.com>
2020-09-27 14:40:42 +0800

    Feats
    更新数据连接的Demo

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,9 @@ Page({
     motto: 'A Demo',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    user: {},
+    vis:false
   },
   //事件处理函数
   bindViewTap: function () {
@@ -52,21 +54,26 @@ Page({
     })
   },
   getcheck: function (e) {
-    var id=e.currentTarget.dataset.userid
+    var that = this
+    var id = e.currentTarget.dataset.userid
     console.log(id)
     wx.request({
-      url: 'http://localhost:8080/api/user', //这里填写你的接口路径
+      url: 'http://10.21.234.24:8080/api/user', //这里填写你的接口路径
       method: 'GET',
       header: { //这里写你借口返回的数据是什么类型，这里就体现了微信小程序的强大，直接给你解析数据，再也不用去寻找各种方法去解析json，xml等数据了
         'Content-Type': 'application/json'
       },
       data: { //这里写你要请求的参数
-        userId:"1"
+        userId: id
       },
       success: function (res) {
         //这里就是请求成功后，进行一些函数操作
         console.log(res.data)
-        return res
+        that.setData({
+            user: res.data,
+            vis:true
+          })
+          // console.log(that.data.user)
       }
     })
   }
