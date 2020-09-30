60156d1fc1fa3d06137d9b2df20ce732d37b6564
Drgil666 <2574105038@qq.com>
2020-09-30 10:39:20 +0800

    Feats
    新增图片转换为Base64的功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,7 +9,9 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     user: {},
-    vis:false
+    uservis: false,
+    photostring: '',
+    photovis: false,
   },
   //事件处理函数
   bindViewTap: function () {
@@ -17,34 +19,7 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
+  onLoad: function () {},
   getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
@@ -55,6 +30,10 @@ Page({
   },
   getcheck: function (e) {
     var that = this
+    that.setData({
+      user: {},
+      uservis: true
+    })
     var id = e.currentTarget.dataset.userid
     console.log(id)
     wx.request({
@@ -70,11 +49,34 @@ Page({
         //这里就是请求成功后，进行一些函数操作
         console.log(res.data)
         that.setData({
-            user: res.data,
-            vis:true
-          })
-          // console.log(that.data.user)
+          user: res.data,
+          uservis: true
+        })
+        // console.log(that.data.user)
       }
+    })
+  },
+  takePhoto: function () {
+    const ctx = wx.createCameraContext()
+    ctx.takePhoto({
+      quality: 'high',
+      success: (res) => {
+        console.log(wx.getFileSystemManager().readFileSync(res.tempImagePath, "base64"))
+      }
+    })
+  },
+  transformBase: function () {
+    let that = this;
+    that.setData({
+      photovis: false,
+      photostring: ''
+    })
+    var FSM = wx.getFileSystemManager().readFileSync('pages/index/test1.png', 'base64');
+    console.log(FSM)
+    //循环将得到的图片转换为Base64
+    that.setData({
+      photovis: true,
+      photostring: FSM
     })
   }
 })
