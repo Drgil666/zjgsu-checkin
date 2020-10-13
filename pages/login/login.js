900ec72af6e6b7e658520555525fbf10be164fcf
Drgil666 <2574105038@qq.com>
2020-10-13 17:49:24 +0800

    Feats
    更新登陆功能和个人主页获取信息功能

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,6 +1,8 @@
 Page({
     data:{
-      hidden:true
+      hidden:true,
+      userid:null,
+      openid:null
     },
     onLoad:function(){
       var that=this
@@ -11,7 +13,6 @@ Page({
       success: function (res) {
           console.log(res)
            if (res.code) {
-              console.log('通过login接口的code换取openid')
                wx.request({
                  url: 'https://api.weixin.qq.com/sns/jscode2session',
                  data: {
@@ -23,16 +24,42 @@ Page({
                  method: 'GET',
                  header: { 'content-type': 'application/json'},
                  success: function(openIdRes){
-                   wx.setStorage({
-                     data: openIdRes.data.openid,
-                     key: 'userid',
-                   })
                    that.setData({
-                    hidden:true
+                    hidden:true,
                   })
                       console.info("登录成功返回的openId：" + openIdRes.data.openid);
-                      wx.switchTab({
-                        url: '../index/index',
+                      wx.setStorageSync('openid', openIdRes.data.openid)
+                      wx.request({
+                        url: 'http://10.21.234.24:8080/login', //这里填写你的接口路径
+                        method: 'POST',
+                        header: { //这里写你借口返回的数据是什么类型，这里就体现了微信小程序的强大，直接给你解析数据，再也不用去寻找各种方法去解析json，xml等数据了
+                          'Content-Type': 'application/json'
+                        },
+                        data: { //这里写你要请求的参数
+                          username: openIdRes.data.openid
+                        },
+                        success: function (res) {
+                          if(res.data.code===0)
+                          {
+                            that.setData({
+                              userid:res.data.data
+                            })
+                            console.log(that.data.userid)
+                            wx.setStorage({
+                              data: that.data.userid,
+                              key: 'userid',
+                            })
+                            wx.switchTab({
+                              url: '../index/index',
+                            })
+                          }
+                          else
+                          {
+                            wx.showToast({
+                              title: res.data.msg,
+                            })
+                          }
+                        }
                       })
                  },
                  fail: function(error) {
@@ -41,6 +68,8 @@ Page({
                   })
                   wx.showToast({
                     title: '获取用户openId失败',
+                    icon:'none',
+                    duration:2000
                   })
                      console.info(error);
                  }
@@ -58,9 +87,5 @@ Page({
               console.log(res)
           }
         })
-    },
-    getopenid:function()
-    {
-        
     }
 })
--- pages/my/my.js
+++ pages/my/my.js
@@ -5,15 +5,43 @@ Page({
    * 页面的初始数据
    */
   data: {
-    userinfo:{ }
+    userid:null,
+    username:null,
+    stuNo:null,
+    mail:null,
+    status:null,
+    photoId:null,
+    nick:null,
+    avatar:null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    this.setData({
-       userinfo: wx.getStorageSync('userInfo')
+  onLoad: function () {
+    var userid=wx.getStorageSync('userid')
+    console.log("userid:"+userid)
+    wx.request({
+      url: 'http://10.21.234.24:8080/api/user', //这里填写你的接口路径
+      method: 'GET',
+      header: { //这里写你借口返回的数据是什么类型，这里就体现了微信小程序的强大，直接给你解析数据，再也不用去寻找各种方法去解析json，xml等数据了
+        'Content-Type': 'application/json'
+      },
+      data: { //这里写你要请求的参数
+        userId: userid
+      },
+      success: function (res) {
+        if(res.data.code===0)
+        {
+          console.log(res.data)
+        }
+        else
+        {
+          wx.showToast({
+            title: res.data.msg,
+          })
+        }
+      }
     })
   },
 
