e9cdf30a7247111d557e0fab055159360ff8f982
Drgil666 <2574105038@qq.com>
2020-11-18 16:09:34 +0800

    Feats:新增显示二维码功能

--- pages/getCheckin/getCheckin.js
+++ pages/getCheckin/getCheckin.js
@@ -4,7 +4,8 @@ Page({
         checkInId: 2,
         role: null,
         checkIn: {},
-        buttonVisible: true
+        buttonVisible: true,
+        qrCode: null
     },
     onLoad: function (e) {
         let that = this
@@ -79,8 +80,9 @@ Page({
                 if (res.data.code === 200) {
                     console.log(res.data.data)
                     that.setData({
-                        checkIn: res.data.data
+                        qrCode: res.data.data
                     })
+                    that.getBase64ImageUrl()
                 } else {
                     wx.showToast({
                         title: res.data.msg
@@ -95,5 +97,17 @@ Page({
             }
         })
 
-    }
+    },
+    //把base64转换成图片
+    getBase64ImageUrl: function () {
+        let that = this
+        let data = "iVBORw0KGgoAAAANSUhEUgAAAMgAAADIAQAAAACFI5MzAAABu0lEQVR42u2YQYrDMAxFVbLwMkfITZKLFWrwxdKb+AhdehGi+V+mIR2Y5fwOQ7MItl8XQvr6Vmr+02Mf8tdJNrPRa/KHTX73UrEfpKS47yBtxt4u7tXjTEmqzWOpzcY8NbMbV+8gbotv5mv/zRvIZu3yuE543d5Aen3Sbtce2/fK/ToJjSIn8/P1ql4BiWeztNpQfbXbtw6WEJuxh0IKU4Sw1lFL8pTu6E4LcbBZoRAtcUdEZm3pGh0qo5SSEGU2uCXCWrCiTNQkdNG7BFFSK1ICq2ZO2gU5gVZwbexq4omxtX5xcXs/ukRDCuWJxFAmzqsT9VGTvuQh4NXkBDbFZoVlZgbIPFEwQhKxZRZqm+hV9VCIjnBwYp/2W/OsEA1xp0fCLuAUifNbSFZJClsURYJGkZ0xv2hHRHBJ4drgTQXThkxPfq0hyAn6NFGt0azhWkoSvYEBennaBYxjkJJ4KFTOUTw8+lRFYn7LrArrQ8EsDy3pM6yzKtRKOXmVinCOz1OMkf2ZzxO+itAyGdE6wjLXc2wq4v3qbAwt5igpYX02HnKOx8oOrxKR/l3P2QVj5N6/KLXk8y/OPyNffMLU6FPXmbUAAAAASUVORK5CYII="
+        var base64Data = data;
+        base64Data = wx.arrayBufferToBase64(wx.base64ToArrayBuffer(base64Data));
+        const base64ImgUrl = "data:image/png;base64," + base64Data;
+        that.setData({
+            qrCode: base64ImgUrl
+        })
+        // console.log(base64ImgUrl)
+    },
 })
